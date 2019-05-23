61cc0cd4bb039d305315209f673400b4e789ab7a
NaokiShigenaga <n-shigenaga@i-enter.co.jp>
2019-05-23 10:14:54 +0900

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.1'
+
+target 'TalkChat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TalkChat
+  pod 'Firebase','5.16.0'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+
+end
---
+++ TalkChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TalkChat
+//
+//  Created by 繁永 直希 on 2019/05/22.
+//  Copyright © 2019 naoki.shigenaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
