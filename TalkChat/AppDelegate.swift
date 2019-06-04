eaefa2fc11c73de1da4f24da349be06ce77c4934
NaokiShigenaga <n-shigenaga@i-enter.co.jp>
2019-06-04 16:20:20 +0900

    スキーマ（URL）による招待コードの実装

--- TalkChat/AppDelegate.swift
+++ TalkChat/AppDelegate.swift
@@ -35,6 +35,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("url : \(url.absoluteString)")
+        print("scheme : \(url.scheme!)")
+        print("host : \(url.host!)")
+        print("port : \(url.port!)")
+        print("query : \(url.query!)")
+        
+        if let slideMenuController = self.window?.rootViewController as? SlideMenuController {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let mainVC = storyboard.instantiateViewController(withIdentifier: "Talk") as? TalkViewController {
+                mainVC.RoomId = url.query!
+                slideMenuController.changeMainViewController(mainVC, close: true)
+            }
+        }
+        
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
