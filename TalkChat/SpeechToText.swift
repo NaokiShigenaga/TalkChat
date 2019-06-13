88cc2e81733d120005a106df54df82ca7b53fd10
NaokiShigenaga <n-shigenaga@i-enter.co.jp>
2019-06-13 17:40:21 +0900

    音声認識のデータ取得方法の改修

--- TalkChat/SpeechToText.swift
+++ TalkChat/SpeechToText.swift
@@ -33,7 +33,9 @@ class SpeechToText {
         startCount += 1
         print(startCount)
         guard let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP")) else { return }
+        
         recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        recognitionRequest.shouldReportPartialResults = true
         audioEngine = AVAudioEngine()
         
         let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
@@ -47,31 +49,56 @@ class SpeechToText {
             return
         }
         
-        
         recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
-            //self.stop()
-            self.delegate?.speechEnd(text: result?.bestTranscription.formattedString ?? "")
-            print("resultHandler")
-            print(result?.bestTranscription.formattedString ?? "")
             
+            var isFinal = false
+            var resultText = ""
             
             if let error = error {
                 print("ERROR!")
                 print(error.localizedDescription)
+                return
+            }
+            
+            print("認識データ：")
+            print(result?.bestTranscription.formattedString ?? "")
+            
+//            if let result = result {
+//                resultText = result.bestTranscription.formattedString
+//                isFinal = result.isFinal
+//            }
+            
+            if let result = result {
+                resultText = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+                self.stop()
+                self.restart()
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4, execute: {
+//                    self.start()
+//                })
+            }
+            
+            if error != nil || isFinal {
+                self.delegate?.speechEnd(text: resultText)
             }
         }
-        
-                DispatchQueue.main.asyncAfter(deadline: .now() + 59.0, execute: {
-                    self.recognitionTask?.cancel()
-                    self.recognitionTask?.finish()
-                    self.audioEngine.stop()
-                    print("RESTART")
-                    self.start()
-                })
+    }
+    
+    func restart() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4, execute: {
+            self.recognitionTask?.cancel()
+            self.recognitionTask?.finish()
+            self.stop()
+            print("RESTART")
+            self.start()
+        })
     }
     
     func stop(){
-        self.audioEngine?.stop()
+        if audioEngine.isRunning {
+            self.recognitionRequest?.endAudio()
+            self.audioEngine?.stop()
+        }
         print("停止ボタンが押されました！")
     }
 }
